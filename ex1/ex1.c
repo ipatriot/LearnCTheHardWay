97a85c2bb6e9e65e6b6eebb79c77175841caaeb9
ipatriots <nicolin16@gmail.com>
2015-11-11 14:17:01 -0500

    Work on Ex1

--- ex1/ex1.c
+++ ex1/ex1.c
@@ -5,10 +5,12 @@ int main(int argc, char *argv[])
 {
   int distance = 100;
   int age = 21;
+  int hey = 12;
 
   //this is also a comment
   printf("You are %d miles away. \n", distance);
   printf("You are %d years old. \n", age);
+  printf("Hey %d \n", hey);
 
   return 0;
 }
