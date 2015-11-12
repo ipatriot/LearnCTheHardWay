79adadf167d81ea6356293eb1c7237553b14c406
ipatriots <nicolin16@gmail.com>
2015-11-11 23:38:07 -0500

    understanding strings and variables

--- ex1/ex1.c
+++ ex1/ex1.c
@@ -6,11 +6,23 @@ int main(int argc, char *argv[])
   int distance = 100;
   int age = 21;
   int hey = 12;
+  float decimal = 10.34;
+  char name[8] = "woo woo";
+  char lots_of_characters[1000] = "lkdflkasdjflasdjfl;dasjflasdjdfs";
+  char one[1] = "d";
 
   //this is also a comment
   printf("You are %d miles away. \n", distance);
   printf("You are %d years old. \n", age);
   printf("Hey %d \n", hey);
+  printf("Hey you! \n");
+  printf("Hey %s how are you? \n", name);
+  printf("Random thought %s woo \n", lots_of_characters);
+  printf("What is the number of life? %f \n", decimal);
+  printf("Woo %c \n", one[0]);
+  printf("This is a new thing, this is one character: %c \n", lots_of_characters[4]);
+  printf("This is a decimal %f and a string %s \nWe just combined variables! jajaja \n", decimal, name);
+
 
   return 0;
 }
