143636982d7d1a2c869495a6a5c0eb9be74714f9
ipatriots <nicolin16@gmail.com>
2015-12-08 00:46:32 -0600

    Working on understanding C booleans and argc argv

--- ex8/ex8.c
+++ ex8/ex8.c
@@ -3,17 +3,21 @@
 int main(int argc, char *argv[])
 {
 	int i = 0;
+	
 
-	if (argc == 1) {
+	if (argc == 2) {
 		printf("You only have one argument\n");
-	} else if (argc > 1 && argc < 4) {
+	} else if (argc > 2 && argc < 5) {
 		printf("Here's you're arguments: \n");
 
 		for (i = 0; i < argc; i++) {
-			printf("%s ", argv[i]);
+			printf("%s \n", argv[i]);
 		}
-		printf("\n");
-	} else {
+		// printf("\n");
+	} else if (argc == 1) {
+		printf("You have 0 arguments. \n");
+	}
+	else {
 		printf("You have to many arguments\n");
 	}
 
